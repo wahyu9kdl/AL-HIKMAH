b9954e8cc004d0648b757ae9a0e0e72992e6be9b
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2021-12-09 23:26:04 +0700

    Create alhikmah.js

---
+++ JS/alhikmah.js
@@ -0,0 +1,23 @@
+ {
+      "kind": "blogger#blog",
+      "id": "392213471435482065",
+      "status": "LIVE",
+      "name": "ALHIKMAH.MY.ID",
+      "description": "𝖆𝖑 𝖍𝖎𝖐𝖒𝖆𝖍",
+      "published": "2020-07-30T22:28:16+07:00",
+      "updated": "2021-12-09T17:25:39+07:00",
+      "url": "http://www.alhikmah.my.id/",
+      "selfLink": "https://content-blogger.googleapis.com/v3/blogs/392213471435482065",
+      "posts": {
+      "totalItems": 465,
+      "selfLink": "https://content-blogger.googleapis.com/v3/blogs/392213471435482065/posts"
+      },
+      "pages": {
+      "totalItems": 29,
+      "selfLink": "https://content-blogger.googleapis.com/v3/blogs/392213471435482065/pages"
+      },
+      "locale": {
+      "language": "in",
+      "country": "",
+      "variant": ""
+      }
